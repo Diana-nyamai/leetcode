5c43a6157806d5b498d666980901d14ce8ec7ba6
Diana-nyamai <dnyamai.dn@gmail.com>
2022-08-16 07:48:46 +0300

    longest prefix

---
+++ LongestP.js
@@ -0,0 +1,34 @@
+function longestCommonPrefix(a)
+{
+    let size = a.length;
+
+    /* if size is 0, return empty string */
+    if (size == 0)
+        return "";
+
+    if (size == 1)
+        return a[0];
+
+    /* sort the array of strings in ascending order*/
+    a.sort();
+
+    /* find the minimum length from first and last string */
+    let end = Math.min(a[0].length, a[size-1].length);
+
+    /* find the common prefix between the first and last string */
+    let i = 0;
+    while (i < end && a[0][i] == a[size-1][i] )
+        i++;
+
+    let pre = a[0].substring(0, i);
+    return pre;
+}
+ 
+/* Driver Function to test other function */
+
+let array=["geeksforgeeks", "geeks", "geek", "geezer"];
+var e = longestCommonPrefix(array);
+document.getElementById('root').innerHTML = e;
+
+
+
